059e64403dbf640d0eefa368d04ebab192d6579d
Darren Jeffrey <darrenjeffrey@hotmail.com>
2023-03-19 22:24:43 -0600

    bump for release

--- gedcom-to-map/const.py
+++ gedcom-to-map/const.py
@@ -1,6 +1,6 @@
 """Constants for gedcom-to-visualmap"""
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 NAME = "gedcom-to-visualmap"
 
 GV_COUNTRIES_JSON = 'https://raw.githubusercontent.com/nnjeim/world/master/resources/json/countries.json'
